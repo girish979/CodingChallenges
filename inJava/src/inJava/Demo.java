f00ecc722d97c940b5e3d25cc266b097017ffcdc
Girish <girish979@gmail.com>
2018-02-10 12:57:53 -0500

    updated

---
+++ inJava/src/inJava/Demo.java
@@ -0,0 +1,21 @@
+package inJava;
+
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in); 
+		
+		String inputString = scan.nextLine(); 
+				
+		System.out.println(inputString);
+		scan.close(); 
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
--- inJava/src/utils/BinarySearch.java
+++ inJava/src/utils/BinarySearch.java
@@ -1,4 +1,4 @@
-package utilities;
+package utils;
 
 public class BinarySearch {
 	static int binarySearch(int arr[], int x)
